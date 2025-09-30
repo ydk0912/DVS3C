0ccf708a07e90a921dc4b0d56bb3521f0e07fb1e
ydk0912 <2571771376@qq.com>
2025-09-30 11:05:49 +0800

    Update updateC.m

--- updateC.m
+++ updateC.m
@@ -1,7 +1,5 @@
 function [U] = updateC(A,m,w,num)
 islocal=1;
-% lambda=1;
-% dist = L2_distance_1(F', F');
 U = zeros(num);
 for i=1:num
     idx = zeros();
@@ -18,10 +16,6 @@ for i=1:num
     for v = 1:m
         a1 = A{v}(i,:)*w(v);
         ai = a1(idxs0);
-%         di = dist(i,idxs0);
-%         mw = m*w(v);
-%         lmw = lambda/mw;
-%         q(v,:) = ai-0.5*lmw*di;
         q(v,:) = ai/sum(w);
     end
     U(i,idxs0) = SloutionToP19(q,m);
@@ -29,18 +23,3 @@ for i=1:num
 end
 end
 
-%   function C = updateC(AV, num_view,Wv, num_samp)
-%         C = zeros(num_samp);
-%         for i = 1:num_samp
-%             zv0 = zeros(1, num_samp);
-%             for v = 1:num_view
-%                 temp = AV{v};
-%                 zv0 = zv0 + Wv(v)*temp(i,:);
-%             end
-%             idxa0 = find(zv0>0);
-%             zi = zv0(idxa0);
-% %             ui = dist(i, idxa0);
-%             cu = (zi)/sum(Wv);
-%             C(i,idxa0) = EProjSimplex_new(cu);
-%         end
-%     end
